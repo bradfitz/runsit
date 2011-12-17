bd4e9193b1c341105110febeb88853b364690df6
Brad Fitzpatrick <brad@danga.com>
2011-12-17 14:09:36 -0800

    crash and status handlers

--- danga.com/runsit/test/daemon/testdaemon.go
+++ danga.com/runsit/test/daemon/testdaemon.go
@@ -19,19 +19,51 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 var port = flag.Int("port", 8000, "port")
 
+func crashHandler(w http.ResponseWriter, r *http.Request) {
+	status := 2
+	if st := r.FormValue("status"); st != "" {
+		status, _ = strconv.Atoi(st)
+	}
+	fmt.Fprintf(os.Stderr, "crashing with status %d", status)
+	os.Exit(status)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "pid=%d\n", os.Getpid())
+	cwd, _ := os.Getwd()
+	fmt.Fprintf(w, "cwd=%s\n", cwd)
+	fmt.Fprintf(w, "uid=%d\n", os.Getuid())
+	fmt.Fprintf(w, "euid=%d\n", os.Geteuid())
+	fmt.Fprintf(w, "gid=%d\n", os.Getgid())
+
+	for _, env := range os.Environ() {
+		fmt.Fprintf(w, "%s\n", env)
+	}
+
+}
+
+func logNoise() {
+	for {
+		log.Printf("some log noise")
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
+	fmt.Fprintf(os.Stdout, "Hello on stdout; listening on port %d\n", *port)
 	fmt.Fprintf(os.Stderr, "Hello on stderr\n")
-	fmt.Fprintf(os.Stdout, "Hello on stdout\n")
-	http.HandleFunc("/crash", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(os.Stderr, "crashing")
-		os.Exit(2)
-	})
+	go logNoise()
+	http.HandleFunc("/crash", crashHandler)
+	http.HandleFunc("/", statusHandler)
 	http.ListenAndServe(":"+strconv.Itoa(*port), nil)
 }
